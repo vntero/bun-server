840c71382ed9bc77df26ec0ce1697eb5b6e89a1d
vntero <shin.antero@gmail.com>
2023-09-12 18:08:14 +0200

    adds more routes

--- index.ts
+++ index.ts
@@ -5,17 +5,19 @@ import { createContact, readContacts, updateContact, deleteContact } from './db'
 // ----- Initiates the handler -----
 const app = new Elysia()
 
+  // ----- Home Page -----
+  .get('/', () => 'Hello Worlk')
   // ----- Create contact -----
-	.get('/create', () => 'Hello Elysia')
+	.get('/create', () => createContact('FIXME'))
 
   // ----- Read all contacts -----
-  .get('/read', () => 'pong')
+  .get('/read', () => readContacts())
 
   // ----- Update contact -----
-  .get('/update', () => 'kadabra')
+  .get('/update', () => updateContact('FIXME', 'FIXME'))
 
   // ----- Delete contact -----
-  .get('/delete', () => 'yang')
+  .get('/delete', () => deleteContact('FIXME'))
 
   // ----- Tell which port is accessible -----
 	.listen(8080)
