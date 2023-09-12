1191c3b5a95f3256824fc64c49285e7287e04940
vntero <shin.antero@gmail.com>
2023-09-12 16:51:54 +0200

    adds db file and 2 queries

--- db.ts
+++ db.ts
@@ -1,1 +1,28 @@
 import { Database } from 'bun:sqlite'
+
+export interface Contact {
+  id?: number,
+  name: string,
+  phone: number,
+  email: string,
+}
+
+export const initDatabase = async (db: Database) => {
+  await db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT)')
+}
+
+export const getContacts = async () => {
+  const db = new Database('contacts.db')
+  await initDatabase(db)
+
+  return db.query('SELECT * FROM contacts').all()
+}
+
+export const addContact = async (contact: Contact) => {
+  const db = new Database('contacts.db')
+  await initDatabase(db)
+
+  const result = await db.query('INSERT INTO contacts (name, phone, email) VALUES (?, ?) RETURNING id').get(contact.name, contact.phone, contact.email) as Contact
+
+  return { ...contact, id: result.id } as Contact
+}
