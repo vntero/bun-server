2323f857cc8bed22be766d81a27e2213b578aca6
vntero <shin.antero@gmail.com>
2023-09-12 17:56:35 +0200

    adds dummy routes in index file

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 import { Elysia } from 'elysia'
 import { html } from '@elysiajs/html'
 import { createContact, readContacts, updateContact, deleteContact } from './db'
+
+// ----- Initiates the handler -----
+const app = new Elysia()
+
+  // ----- Create contact -----
+	.get('/create', () => 'Hello Elysia')
+
+  // ----- Read all contacts -----
+  .get('/read', () => 'pong')
+
+  // ----- Update contact -----
+  .get('/update', () => 'kadabra')
+
+  // ----- Delete contact -----
+  .get('/delete', () => 'yang')
+
+  // ----- Tell which port is accessible -----
+	.listen(8080)
+	 
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
